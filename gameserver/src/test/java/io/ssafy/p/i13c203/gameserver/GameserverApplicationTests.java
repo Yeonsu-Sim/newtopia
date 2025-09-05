eb8c671f8a2acbdba4b93650ca4d5c2b5395ae55
문영호 <dudgh614@gmail.com>
2025-09-05 10:57:58 +0900

    chore: initialize project with Spring Boot

---
+++ gameserver/src/test/java/io/ssafy/p/i13c203/gameserver/GameserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssafy.p.i13c203.gameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
