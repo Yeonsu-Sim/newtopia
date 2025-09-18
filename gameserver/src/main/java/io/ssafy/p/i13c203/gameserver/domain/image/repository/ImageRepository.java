6270ba0ce3db0cdc7f06000371331464f89085b5
심연수 <shenlianxiu@gmail.com>
2025-09-18 17:07:26 +0900

    fix: 이미지 시드 데이터 변경에 대한 대응
    - postgresSQL의 image 테이블 관리
      - 기존에 minio의 이미지 객체를 참조하던 인스턴스가 있다면, 새로 추가하지 않음
    - 이유: 중복 이미지 추가 방지

--- gameserver/src/main/java/io/ssafy/p/i13c203/gameserver/domain/image/repository/ImageRepository.java
+++ gameserver/src/main/java/io/ssafy/p/i13c203/gameserver/domain/image/repository/ImageRepository.java
@@ -1,10 +1,11 @@
 package io.ssafy.p.i13c203.gameserver.domain.image.repository;
 
 import io.ssafy.p.i13c203.gameserver.domain.image.entity.Image;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
-
+    Optional<Image> findByStorageKey(String storageKey);
 }
